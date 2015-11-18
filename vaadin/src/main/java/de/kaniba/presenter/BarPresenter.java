bd72d46a2aac9004f7b739a2e055938d51ed83d4
Zuseban <zander-92@web.de>
2015-11-18 18:00:01 +0100

    Created New Classes and Package
    New Packages for view,model and presenter. Added classes and interfaces.

---
+++ vaadin/src/main/java/de/kaniba/presenter/BarPresenter.java
@@ -0,0 +1,13 @@
+package de.kaniba.presenter;
+
+import de.kaniba.view.BarView;
+
+public class BarPresenter implements BarView.BarViewListener{
+
+	@Override
+	public void buttonClick(char operation) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ vaadin/src/main/java/de/kaniba/view/BarViewImpl.java
@@ -0,0 +1,38 @@
+package de.kaniba.view;
+
+import com.vaadin.event.MouseEvents.ClickEvent;
+import com.vaadin.event.MouseEvents.ClickListener;
+import com.vaadin.ui.CustomComponent;
+import de.kaniba.view.*;
+
+public class BarViewImpl extends CustomComponent implements BarView, ClickListener{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5449042042425044956L;
+
+	public BarViewImpl() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void setDisplay(double value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addListener(BarViewListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void click(ClickEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
