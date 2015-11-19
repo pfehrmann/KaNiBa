cdc38d26c56a44487f2a9cc28084d3d70cd73fc5
Zuseban <zander-92@web.de>
2015-11-19 11:30:05 +0100

    UML Class diagram
    Actual UML Class Diagramm

--- vaadin/src/main/java/de/kaniba/presenter/BarPresenter.java
+++ vaadin/src/main/java/de/kaniba/presenter/BarPresenter.java
@@ -1,9 +1,11 @@
 package de.kaniba.presenter;
 
+import de.kaniba.model.Bar;
 import de.kaniba.view.BarView;
 
 public class BarPresenter implements BarView.BarViewListener{
 
+	Bar bar;
 	@Override
 	public void buttonClick(char operation) {
 		// TODO Auto-generated method stub
