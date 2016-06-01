76a02d1558a49885a8f221c24338194eae5cd21e
Philipp Fehrmann <philipp-09@live.de>
2016-06-01 20:23:55 +0200

    Added data validation for the register view

--- vaadin/src/main/java/de/kaniba/view/RegisterView.java
+++ vaadin/src/main/java/de/kaniba/view/RegisterView.java
@@ -1,11 +1,19 @@
 package de.kaniba.view;
 
+import java.sql.Date;
+import java.time.Instant;
+import java.util.Iterator;
+
 import com.vaadin.data.Validator;
+import com.vaadin.data.validator.DateRangeValidator;
 import com.vaadin.data.validator.EmailValidator;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.server.Page;
+import com.vaadin.shared.ui.datefield.Resolution;
+import com.vaadin.ui.AbstractField;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Component;
 
 import de.kaniba.designs.RegisterDesign;
 import de.kaniba.model.Address;
@@ -16,25 +24,36 @@ import de.kaniba.uiInterfaces.RegisterViewInterface;
 
 /**
  * The view for registering
+ * 
  * @author Philipp
  *
  */
 public class RegisterView extends RegisterDesign implements RegisterViewInterface {
+	/**
+	 * 
+	 */
+	private static final long SECONDS_PER_YEAR = (long) 3600 * 24 * 365;
+
+	/**
+	 * 
+	 */
+	private static final int MINIMUM_AGE = 12;
+
 	private static final long serialVersionUID = 1L;
 
 	public static final String NAME = "register";
-	
+
 	private RegisterPresenterInterface presenter;
-	
+
 	/**
 	 * Set the view up.
 	 */
-	public RegisterView() {		
+	public RegisterView() {
 		emailField.addValidator(new EmailValidator("Du musst eine gültige Email Addresse eingeben."));
-		
+
 		Validator passwordValidator = new RepeatPasswordValidator(passwordField, repeatPasswordField);
 		repeatPasswordField.addValidator(passwordValidator);
-		
+
 		submitButton.addClickListener(new Button.ClickListener() {
 			private static final long serialVersionUID = 1L;
 
@@ -43,9 +62,19 @@ public class RegisterView extends RegisterDesign implements RegisterViewInterfac
 				presenter.registerClick();
 			}
 		});
+		java.util.Date maximumAge = Date.valueOf("1900-1-1");
+		java.util.Date minimumAge = Date.from(Instant.now().minusSeconds(MINIMUM_AGE * SECONDS_PER_YEAR));
+
+		birthdateField.addValidator(
+				new DateRangeValidator("Du bist entweder zu alt für diesen Scheiß, oder aus der Zukunft...", maximumAge,
+						minimumAge, Resolution.DAY));
+		birthdateField.setRangeStart(maximumAge);
+		birthdateField.setRangeEnd(minimumAge);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see de.kaniba.view.RegisterViewInterface#getSubmit()
 	 */
 	@Override
@@ -58,19 +87,24 @@ public class RegisterView extends RegisterDesign implements RegisterViewInterfac
 		Page.getCurrent().setTitle("Registrieren");
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see de.kaniba.view.RegisterViewInterface#getUser()
 	 */
 	@Override
 	public InternalUser getUser() {
+		if (!checkFields()) {
+			return null;
+		}
 		InternalUser user = new InternalUser();
 
 		Address address = new Address(cityField.getValue(), streetField.getValue(), numberField.getValue(),
 				zipField.getValue());
 		Email email = new Email(emailField.getValue());
 		java.util.Date utilDate = birthdateField.getValue();
-	    java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
-	    
+		java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+
 		user.setFirstname(firstNameField.getValue());
 		user.setName(nameField.getValue());
 		user.setPassword(passwordField.getValue());
@@ -81,8 +115,33 @@ public class RegisterView extends RegisterDesign implements RegisterViewInterfac
 		return user;
 	}
 
-	/* (non-Javadoc)
-	 * @see de.kaniba.view.RegisterViewInterface#setPresenter(de.kaniba.presenter.RegisterPresenterInterface)
+	/**
+	 * @return
+	 */
+	private boolean checkFields() {
+		Iterator<Component> iterator = registerLayout.iterator();
+
+		while (iterator.hasNext()) {
+			Component component = iterator.next();
+
+			if (component instanceof AbstractField) {
+				@SuppressWarnings("rawtypes") //NOSONAR
+				AbstractField field = (AbstractField) component;
+
+				if (!field.isValid()) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * de.kaniba.view.RegisterViewInterface#setPresenter(de.kaniba.presenter.
+	 * RegisterPresenterInterface)
 	 */
 	@Override
 	public void setPresenter(RegisterPresenterInterface presenter) {
